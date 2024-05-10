387972ec2bf5b6fdf0eb7fc051ab0e89a7abca5a
Shea Levy <shea@shealevy.com>
2024-05-10 05:33:18 -0400

    Switch to LVM for root disk

--- disk.nix
+++ disk.nix
@@ -1,38 +1,64 @@
 {
   # TODO: Move data partitions to a single bcachefs once https://github.com/nix-community/disko/issues/511 is fixed
-  disko.devices.disk = {
-    disk0 = {
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            size = "1M";
-            type = "EF02"; # grub MBR
-          };
+  disko.devices = {
+    disk = {
+      disk0 = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            mbr = {
+              size = "1M";
+              type = "EF02"; # grub MBR
+            };
 
-          root = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
+            boot = {
+              size = "2G";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/boot";
+              };
+            };
+
+            pv = {
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "pool";
+              };
+            };
+          };
+        };
+      };
+      disk1 = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            pv = {
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "pool";
+              };
             };
           };
         };
       };
     };
-    disk1 = {
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          nix = {
-            size = "100%";
+
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            lvm_type = "raid0";
             content = {
               type = "filesystem";
               format = "ext4";
-              mountpoint = "/nix";
+              mountpoint = "/";
             };
           };
         };
