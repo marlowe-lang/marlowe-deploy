2c3c0f1a86d98be5d1bdaf2beb76b2607a3b8997
paluh (aka Tomasz Rybarczyk) <paluho@gmail.com>
2025-11-08 15:12:18 +0100

    Add konduit adaptor configuration to the secrets

--- secrets.nix
+++ secrets.nix
@@ -9,7 +9,8 @@ let
   all-keys = [ system-key ] ++ admin-keys;
 in {
   "id_ed25519.age".publicKeys = admin-keys;
-  "marlowe-playground/jwt.age".publicKeys = all-keys;
-  "marlowe-playground/gh.age".publicKeys = all-keys;
+  "konduit-adaptor-env-file.age".publicKeys = all-keys;
+  # "marlowe-playground/jwt.age".publicKeys = all-keys;
+  # "marlowe-playground/gh.age".publicKeys = all-keys;
   "hetzner/auth-config.age".publicKeys = admin-keys;
 }
